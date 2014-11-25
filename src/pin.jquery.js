2de4fe41ccd9e7084017abf9d04c22a9685474d7
Jay Salvat <jay@jaysalvat.com>
2014-11-25 12:47:28 +0100

    Add some draft methods to pin.jquery.js

--- src/pin.jquery.js
+++ src/pin.jquery.js
@@ -48,4 +48,70 @@
     $.fn.last = function (key)  {
         return $.eq(this.length - 1);
     };
+
+    $.fn.size = function () {
+        return this.length;
+    };
+
+    $.fn.get = function (idx)  {
+        if (idx === undefined) {
+            return this.toArray();
+        }
+
+        if (idx < 0) {
+            return this[idx + this.length];
+        }
+
+        return this[idx];
+    };
+
+    $.fn.toArray = function () {
+        return [].slice.call(this);
+    };
+
+    $.fn.html = function (html) {
+        if (html === undefined) {
+            return this[0].innerHTML;
+        }
+
+        return this.each(function (i) {
+            this.innerHTML = functionOrValue(value, this, i);
+        });
+    };
+
+    $.fn.text = function (text) {
+        if (text === undefined) {
+            return this[0].innerHTML || this[0].textContent;
+        }
+
+        return this.each(function (i) {
+            this.textContent = functionOrValue(value, this, i);
+        });
+    };
+
+    $.fn.data = function (key, value) {
+        key = camelize(key);
+
+        if (value === undefined) {
+            return this[0].dataset[key];
+        }
+
+        return this.each(function (i) {
+            this.dataset[key] = value;
+        });
+    };
+
+    function functionOrValue (value, elmt, i) {
+        if (typeof value === 'function') {
+            return value.call(elmt, i);
+        }
+
+        return value;
+    }
+
+    function camelize (string) { 
+        return string.replace(/-+(.)?/g, function (match, chr) { 
+            return chr ? chr.toUpperCase() : '';
+        });
+    }
 }(Pin));
