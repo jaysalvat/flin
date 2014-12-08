cb3f185b5f9efc95878627cf782a9f2111b76ffa
Jay Salvat <jay@jaysalvat.com>
2014-12-08 13:25:28 +0100

    Build v0.1.4

---
+++ dist/pin.jquery.js
@@ -0,0 +1,230 @@
+/*! Pin v0.1.4 (c) 2014 Jay Salvat http://pin.jaysalvat.com */
+
+(function ($) {
+    'use strict';
+    /* jshint laxbreak: true */ 
+
+    $.fn.wrap = function (value) {
+        return this.wrapWith(value);
+    };
+
+    $.fn.css = function (key, value) {
+        if (value === undefined) {
+            return this.get(':' + key);
+        }
+        
+        return this.set(':' + key, value);
+    };
+
+    $.fn.attr = function (key, value) {
+        if (value === undefined) {
+            return $(this).get('@' + key);
+        }
+        
+        return $(this).set('@' + key, value);
+    };
+
+    $.fn.removeAttr = function (key, value) {
+        return $(this).set('@' + key);
+    };
+
+    $.fn.addClass = function (key)  {
+        return $(this).set('.' + key);
+    };
+
+    $.fn.removeClass = function (key)  {
+        return $(this).set('.' + key, 'remove');
+    };
+
+    $.fn.toggleClass = function (key)  {
+        return $(this).set('.' + key, 'toggle');
+    };
+
+    $.fn.hasClass = function (key)  {
+        return $(this).get('.' + key);
+    };
+
+    $.fn.first = function (key)  {
+        return $(this).eq(0);
+    };
+
+    $.fn.last = function (key)  {
+        return $(this).eq(this.length - 1);
+    };
+
+    $.fn.size = function () {
+        return this.length;
+    };
+
+    $.fn.toArray = function () {
+        return [].slice.call(this);
+    };
+
+    $.fn.html = function (value) {
+        if (value === undefined) {
+            return this[0].innerHTML;
+        }
+
+        return this.each(function (i) {
+            this.innerHTML = functionOrValue(value, this, i);
+        });
+    };
+
+    $.fn.text = function (value) {
+        if (value === undefined) {
+            return this[0].innerHTML || this[0].textContent;
+        }
+
+        return this.each(function (i) {
+            this.textContent = functionOrValue(value, this, i);
+        });
+    };
+
+    $.fn.val = function (value) {
+        if (value === undefined) {
+            if (this[0].multiple) {
+                return [].map.call(
+                    elmt.querySelectorAll('option[selected'), function (option) { 
+                      return option.value; 
+                    }
+                );
+            }
+            
+            return this[0].value;
+        }
+
+        return this.each(function (i) {
+            this.value = functionOrValue(value, this, i);
+        });
+    };
+
+    $.fn.empty = function () {
+        return this.each(function () {
+            this.innerHTML = '';
+        });
+    };
+
+    $.fn.contents = function () {
+        var elmts = [];
+
+        this.each(function () {
+            elmts = elmts.concat([].slice.apply(this.childNodes));
+        });
+
+        return $($.uniq(elmts));
+    };
+
+    $.fn.data = function (key, value) {
+        key = camelize(key);
+
+        if (value === undefined) {
+            return this[0].dataset[key];
+        }
+
+        return this.each(function (i) {
+            this.dataset[key] = value;
+        });
+    };
+
+    $.fn.show = function () {
+        var args = arguments;
+
+        return this.each(function () {
+            var $elmt = $(this);
+
+            if ($elmt.get(':display') === 'none') {
+                $elmt.set(':display', $elmt.data('_display') || '');
+            }
+
+            findCallbackInArgs(args).apply(this);
+        });
+    };
+
+    $.fn.hide = function () {
+        var args = arguments;
+
+        return this.each(function () {
+            var $elmt = $(this);
+
+            $elmt.data('_display', $elmt.get(':display'));
+            $elmt.set(':display', 'none');
+
+            findCallbackInArgs(args).apply(this);
+        });       
+    };
+
+    $.fn.toggle = function () {
+        var args = arguments;
+        
+        return this.each(function () {
+            var $elmt = $(this);
+
+            if ($elmt.get(':display') === 'none') {
+                $elmt.show();
+            } else {
+                $elmt.hide();
+            }
+
+            findCallbackInArgs(args).apply(this);
+        });  
+    };
+
+    [ 'fadeIn', 'slideDown', 'fadeOut', 'slideUp', 'fadeToggle', 'slideToggle', 'fadeTo' ].forEach(function (name, i) {
+        if (i === 0 || i === 1) {
+            $.fn[name] = function () {
+                return this.show.apply(this, arguments);
+            };
+        }
+
+        if (i === 2 || i === 3) {
+            $.fn[name] = function () {
+                return this.hide.apply(this, arguments);
+            };
+        }
+
+        if (i === 4 || i === 5) {
+            $.fn[name] = function () {
+                return this.toggle.apply(this, arguments);
+            };
+        }
+
+        $.fn[name] = function (duration, opacity) {
+            if (opacity > 0.5) {
+                return this.show.apply(this, arguments);    
+            }
+            return this.hide.apply(this, arguments);
+        };
+    });
+
+    [ 'prependTo', 'appendTo', 'insertBefore', 'insertAfter' ].forEach(function (name, i) {
+        $.fn[name] = function (html) {
+            var method = name.toLowerCase().replace(/to|insert/, '');
+
+            $(html)[method](this);
+        };
+    });
+
+    function findCallbackInArgs (args) {
+        for (var i = 0; i < args.length; i++) {
+            if (typeof args[i] === 'function') {
+                return args[i];
+            }
+        }
+
+        return function () {};
+    }
+
+    function functionOrValue (value, elmt, i) {
+        if (typeof value === 'function') {
+            return value.call(elmt, i);
+        }
+
+        return value;
+    }
+
+    function camelize (string) { 
+        return string.replace(/-+(.)?/g, function (match, chr) { 
+            return chr ? chr.toUpperCase() : '';
+        });
+    }
+}(Pin));
---
+++ dist/pin.lite.js
@@ -0,0 +1,174 @@
+/*! Pin v0.1.4 (c) 2014 Jay Salvat http://pin.jaysalvat.com */
+
+(function (context, factory) {
+    'use strict';
+    /* globals define: true, module: true */
+
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else {
+        context.Pin = factory();
+
+        if (context.$ === undefined) {
+            context.$ = context.Pin;
+        }
+    }
+})(this, function () {
+    'use strict';
+    /* jshint laxbreak: true, loopfunc: true */ 
+
+    var $,
+        pin   = {}, 
+        win   = window,
+        doc   = document,
+        div   = doc.createElement('div'),
+        table = doc.createElement('table'), 
+        tbody = doc.createElement('tbody'),
+        tr    = doc.createElement('tr'),
+        containers = {
+            'thead': table, 
+            'tbody': table, 
+            'tfoot': table,
+            'tr':    tbody,
+            'td':    tr, 
+            'th':    tr,
+            '*':     div
+        },
+        tagRe = /^\s*<(\w+|!)[^>]*>/;
+
+    pin.init = function (selector, context) {
+        var elmts;
+
+        if (!selector) {
+            return new pin.Collection();
+        }
+        else if (selector._pin) {
+            return selector;
+        }
+        else if (selector instanceof Function) {
+            return doc.addEventListener('DOMContentLoaded', selector);
+        }
+        else if (selector instanceof Array) {
+            elmts = selector;
+        }
+        else if (typeof selector === 'string') {
+            selector = selector.trim();
+
+            if (selector[0] === '<') {
+                elmts = pin.fragment(selector);
+            }
+            else if (context) {
+                return $(context).find(selector);
+            } 
+            else {
+                elmts = doc.querySelectorAll(selector);
+            }
+        }
+        else if (selector instanceof NodeList) {
+            elmts = selector;
+        }
+        else if (typeof selector === 'object') {
+            elmts = [ selector ];
+        }
+
+        return new pin.Collection(elmts);
+    };
+
+    pin.Collection = function (elmts) {
+        elmts = [].slice.call(elmts || []);
+        elmts._pin = true;
+
+        $.each($.fn, function (i) {
+            elmts[i] = $.fn[i];
+        });
+
+        return elmts;
+    };
+
+    pin.fragment = function (html) {
+        var container,
+            elmts,
+            name = html.match(tagRe)[1];
+
+        if (!containers[name]) {
+            name = '*';
+        }
+
+        container = containers[name];
+        container.innerHTML = html;
+
+        return $(container.childNodes).each(function () {
+            container.removeChild(this);
+        });
+    };
+
+    $ = function (selector, context) {
+        return pin.init(selector, context);
+    };
+
+    $.each = function (elmts, fn) {
+        var i, k;
+
+        if (typeof elmts.length === 'number') {
+            for (i = 0; i < elmts.length; i++) {
+                if (fn.call(elmts[i], i, elmts[i]) === false) {
+                    return elmts;
+                }
+            }
+        } else {
+            for (k in elmts) {
+                if (elmts.hasOwnProperty(k)) {
+                    if (fn.call(elmts[k], k, elmts[k]) === false) {
+                        return elmts;
+                    }
+                }
+            }
+        }
+
+        return elmts;
+    };
+
+    $.map = function (elmts, fn) {
+        var values = [],
+            value;
+
+        $(elmts).each(function (i) {
+            value = fn.call(this, this, i);
+
+            if (value !== null) {
+                values.push(value);
+            }
+        });
+
+        return values;
+    };
+
+    $.uniq = function (elmts) {
+        return [].filter.call(elmts, function (elmt, idx) { 
+            return elmts.indexOf(elmt) === idx;
+        });
+    };
+
+    $.fn = {
+
+        each: function (callback) {
+            return $.each(this, callback);
+        },
+
+        map: function (callback) {
+            return $($.map(this, callback));
+        },
+
+        find: function (selector) {
+            var elmts = [];
+
+            this.each(function () {
+                elmts = elmts.concat($(this.querySelectorAll(selector)));
+            });
+
+            return $($.uniq(elmts));
+        }
+    };
+
+    return $;
+});
